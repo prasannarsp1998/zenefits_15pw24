c5abc03340c4a0805d1301a2fb0e508d6006661b
Prasanna S <34684080+prasannarsp1998@users.noreply.github.com>
2018-03-19 11:26:53 +0530

    Add files via upload

---
+++ Question_2/dic_rank.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<string>
+using namespace std;
+int permutation(char word,int length ){
+
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    char letters[n];
+    for (int i=0;i<n;i++){
+        cin>>letters[i];
+    }
+    char t_string;
+    cin>>t_string;
+
+    permutation(t_string,(strlen(t_string))-1)
+    return 0;
+}
